e10483d2c11e9a5d2058234e0822ed1639e3ecf8
Abiodun Elijah, Siyanbola <43834958+abiodunelijah@users.noreply.github.com>
2025-08-21 11:56:46 +0100

    Update ContactAddressController.java

--- phonebook/src/main/java/com/coder2client/phonebook/ContactAddressController.java
+++ phonebook/src/main/java/com/coder2client/phonebook/ContactAddressController.java
@@ -1,5 +1,6 @@
 package com.coder2client.phonebook;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,28 +18,25 @@ public class ContactAddressController {
     }
 
     @PostMapping("/create-contact")
-    //url http://localhost:8080/create-contact
-    public ResponseEntity<ContactAddressDTO> createContact(@RequestBody ContactAddressDTO contactAddressDTO){
+    public ResponseEntity<ContactAddressDTO> createContact(@Valid @RequestBody ContactAddressDTO contactAddressDTO){
         ContactAddressDTO contact = contactAddressService.createContact(contactAddressDTO);
         return new ResponseEntity<>(contact, HttpStatus.CREATED);
     }
 
     @GetMapping("/all-contacts")
-    //url http://localhost:8080/all-contacts
     public ResponseEntity<List<ContactAddressDTO>> getAllContactAddresses(){
         List<ContactAddressDTO> allContactAddresses = contactAddressService.getAllContactAddresses();
         return new ResponseEntity<>(allContactAddresses, HttpStatus.OK);
     }
 
     @GetMapping("/contact-address/{id}")
-    //url http://localhost:8080/contact-address/1
     public ResponseEntity<ContactAddressDTO> getContactAddress(@PathVariable Long id){
         ContactAddressDTO contactAddress = contactAddressService.getContactAddress(id);
         return new ResponseEntity<>(contactAddress, HttpStatus.OK);
     }
 
     @PatchMapping("/update-contact/{id}")
-    public ResponseEntity<ContactAddressDTO> updateContactAddress(@PathVariable Long id, @RequestBody ContactAddressDTO contactAddressDTO){
+    public ResponseEntity<ContactAddressDTO> updateContactAddress(@PathVariable Long id, @Valid @RequestBody ContactAddressDTO contactAddressDTO){
         contactAddressDTO.setId(id);
 
         ContactAddressDTO updateContactAddress = contactAddressService.updateContactAddress(contactAddressDTO);
@@ -62,5 +60,4 @@ public class ContactAddressController {
         List<ContactAddressDTO> contactAddressByCriteria = contactAddressService.findContactAddressByCriteria(firstName, lastName, email, phoneNumber);
         return new ResponseEntity<>(contactAddressByCriteria, HttpStatus.FOUND);
     }
-
 }
