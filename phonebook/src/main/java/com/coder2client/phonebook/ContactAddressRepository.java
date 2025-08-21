1f82a15f19d43df8cf74364ccea26c89c087ea3a
Abiodun Elijah, Siyanbola <43834958+abiodunelijah@users.noreply.github.com>
2025-08-21 12:25:09 +0100

    Update ContactAddressRepository.java

--- phonebook/src/main/java/com/coder2client/phonebook/ContactAddressRepository.java
+++ phonebook/src/main/java/com/coder2client/phonebook/ContactAddressRepository.java
@@ -1,6 +1,19 @@
 package com.coder2client.phonebook;
 
+import com.coder2client.phonebook.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContactAddressRepository extends JpaRepository<ContactAddress, Long> {
+    
+    List<ContactAddress> findByUser(User user);
+    
+    List<ContactAddress> findByUserAndFirstNameContainingIgnoreCase(User user, String firstName);
+    
+    List<ContactAddress> findByUserAndLastNameContainingIgnoreCase(User user, String lastName);
+    
+    List<ContactAddress> findByUserAndEmailContainingIgnoreCase(User user, String email);
+    
+    List<ContactAddress> findByUserAndPhoneNumberContaining(User user, String phoneNumber);
 }
